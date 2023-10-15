1abcf2584068a0f5b1c4dba13a8289a3f3a56569
Pooja Seriyala <pooja@Poojas-MacBook-Air.local>
2023-10-16 00:21:16 +0530

    Updates to ios folder

--- ios/Pods/Target Support Files/Pods-prettyLittleThing-prettyLittleThingTests/Pods-prettyLittleThing-prettyLittleThingTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_prettyLittleThing_prettyLittleThingTests : NSObject
-@end
-@implementation PodsDummy_Pods_prettyLittleThing_prettyLittleThingTests
-@end
